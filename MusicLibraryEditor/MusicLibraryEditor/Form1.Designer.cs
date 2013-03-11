5721bf08dad94b79cd79920dbc1f8acaf61119a9
Steven Zehner <steve@szehner.com>
2013-03-11 02:56:08 -0700

    Got Read Working
    Managed to read text from mpe (A Perfect Circle's Pet renamed to
    test.mp3)

--- MusicLibraryEditor/MusicLibraryEditor/Form1.Designer.cs
+++ MusicLibraryEditor/MusicLibraryEditor/Form1.Designer.cs
@@ -28,12 +28,71 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.gbFileList = new System.Windows.Forms.GroupBox();
+            this.gbMetadata = new System.Windows.Forms.GroupBox();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.gbMetadata.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gbFileList
+            // 
+            this.gbFileList.Location = new System.Drawing.Point(12, 12);
+            this.gbFileList.Name = "gbFileList";
+            this.gbFileList.Size = new System.Drawing.Size(178, 416);
+            this.gbFileList.TabIndex = 0;
+            this.gbFileList.TabStop = false;
+            this.gbFileList.Text = "File List";
+            // 
+            // gbMetadata
+            // 
+            this.gbMetadata.Controls.Add(this.txtTitle);
+            this.gbMetadata.Controls.Add(this.lblTitle);
+            this.gbMetadata.Location = new System.Drawing.Point(196, 12);
+            this.gbMetadata.Name = "gbMetadata";
+            this.gbMetadata.Size = new System.Drawing.Size(415, 416);
+            this.gbMetadata.TabIndex = 1;
+            this.gbMetadata.TabStop = false;
+            this.gbMetadata.Text = "Metadata";
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Location = new System.Drawing.Point(6, 30);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(30, 13);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Title:";
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Location = new System.Drawing.Point(42, 27);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(367, 20);
+            this.txtTitle.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(623, 440);
+            this.Controls.Add(this.gbMetadata);
+            this.Controls.Add(this.gbFileList);
+            this.Name = "Form1";
+            this.Text = "Music Library Editor";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.gbMetadata.ResumeLayout(false);
+            this.gbMetadata.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox gbFileList;
+        private System.Windows.Forms.GroupBox gbMetadata;
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.Label lblTitle;
     }
 }
 
--- MusicLibraryEditor/MusicLibraryEditor/Form1.cs
+++ MusicLibraryEditor/MusicLibraryEditor/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using TagLib;
 
 namespace MusicLibraryEditor
 {
@@ -15,6 +16,12 @@ namespace MusicLibraryEditor
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            TagLib.File track = TagLib.File.Create("test.mp3");
+            txtTitle.Text = track.Tag.Title;
         }
     }
 }
